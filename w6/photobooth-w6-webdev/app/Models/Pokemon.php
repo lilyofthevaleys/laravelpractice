9d7218da689b350e5ec1dd1f17d82f9b88b3bb7b
Charlene Athena <charleneathena@gmail.com>
2026-04-20 15:35:16 +0700

    Add soft deletes to pokemons

--- w6/photobooth-w6-webdev/app/Models/Pokemon.php
+++ w6/photobooth-w6-webdev/app/Models/Pokemon.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pokemon extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'pokemons';
 
     protected $fillable = [
